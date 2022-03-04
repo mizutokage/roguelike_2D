1deebbe5d1d9b807bb72c859a90e7a9b2504dd83
mizutokage <51310292+mizutokage@users.noreply.github.com>
2022-03-05 07:47:10 +0900

    seconde commit

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -6,18 +6,26 @@ public class BoardManager : MonoBehaviour
 {
 
     public int colums = 8, rows = 8; // Player移動範囲
-    public GameObject[] FloorTiles; 
-    public GameObject[] OutWallTiles;
+    public GameObject[] floorTiles; 
+    public GameObject[] outWallTiles;
+    public GameObject[] wallTiles;
+    public GameObject[] foodTiles;
+    public GameObject exit;
+
+    private List<Vector3> gridPositions = new List<Vector3>();
+
+    // 壁、アイテムの数
+    public int wallMinimum = 5, wallMaximum = 9, foodMinimum = 1, foodMaximum = 5;
 
     void BoardSetup() { // FloorとWallを設置
         for (int x = -1; x < colums + 1; x++) {
-            for (int y = -1; y < rows + 1; x++) {
+            for (int y = -1; y < rows + 1; y++) {
                 GameObject toInstantiate;
                 if (x == -1 || x == colums || y == -1 || y == rows) {
-                    toInstantiate = OutWallTiles[Random.Range(0, OutWallTiles.Length)]; // Wallを設置
+                    toInstantiate = outWallTiles[Random.Range(0, outWallTiles.Length)]; // Wallを設置
                 }
                 else {
-                    toInstantiate = FloorTiles[Random.Range(0, FloorTiles.Length)]; // Floorを設置
+                    toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)]; // Floorを設置
                 }
 
                 Instantiate(toInstantiate, new Vector3(x, y, 0), Quaternion.identity);
@@ -25,10 +33,51 @@ public class BoardManager : MonoBehaviour
         }
     }
 
+    void InitialiseList() {
+        gridPositions.Clear();
+
+        for (int x = 1; x < colums - 1; x++) {
+            for (int y = 1; y < rows - 1; y++) {
+                gridPositions.Add(new Vector3(x, y, 0)); // アイテムを生成するポジション
+            }
+        }
+    }
+
+    Vector3 RandomPosition() {
+        int randomIndex = Random.Range(0, gridPositions.Count); // リストからランダムに数値を取得
+
+        Vector3 randomPosition = gridPositions[randomIndex]; // 取得した数値のポジションをrandomPositionに代入
+
+        gridPositions.RemoveAt(randomIndex); // 1度使ったポジションを削除、同じポジションに同じアイテムが配置されるのを防ぐ
+
+        return randomPosition;
+    }
+
+    void LayoutobjectAtRandom(GameObject[] tileArray, int min, int max) {
+        int objectCount = Random.Range(min, max + 1); // 生成するアイテムの数
+
+        for (int i = 0; i < objectCount; i++) { 
+            Vector3 randomPosition = RandomPosition();
+
+            GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+
+            Instantiate(tileChoice, randomPosition, Quaternion.identity);
+        }
+    }
+
+    public void SetupScene() {
+        BoardSetup();
+        InitialiseList();
+        LayoutobjectAtRandom(wallTiles, wallMinimum, wallMaximum);
+        LayoutobjectAtRandom(foodTiles, foodMinimum, foodMaximum);
+
+        Instantiate(exit, new Vector3(colums-1, rows-1, 0), Quaternion.identity);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        BoardSetup();
+        SetupScene();
     }
 
     // Update is called once per frame
